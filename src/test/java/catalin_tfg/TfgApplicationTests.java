27ef4898178cecb3956f0fc8fbc38da37293b4a9
catalindumitrache76 <catalindumitrache76@yahoo.es>
2017-02-27 16:49:32 +0100

    Added basic Spring structure

---
+++ src/test/java/catalin_tfg/TfgApplicationTests.java
@@ -0,0 +1,16 @@
+package catalin_tfg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TfgApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
