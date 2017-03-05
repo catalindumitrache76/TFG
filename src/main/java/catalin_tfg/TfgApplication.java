766718cdfeff8a6d3e9606752cb489410e425aa8
catalindumitrache76 <catalindumitrache76@yahoo.es>
2017-03-05 19:51:18 +0100

    Travis

--- src/main/java/catalin_tfg/TfgApplication.java
+++ src/main/java/catalin_tfg/TfgApplication.java
@@ -2,12 +2,9 @@ package catalin_tfg;
 
 import java.io.IOException;
 
-import org.junit.runner.RunWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootApplication
 public class TfgApplication {
 
