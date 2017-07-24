cf44d86f3a74f0e6f2e118a89e8e21299108447c
Catalin <catalindumitrache76@yahoo.es>
2017-07-24 19:14:48 +0200

    Clean project for new DB approach

--- Hive_Tables_Scripts/create_sequences.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS default.sequences
-(
-  id BIGINT,
-  name varchar(50)
-)
-ROW FORMAT DELIMITED Fields terminated by ','
-STORED AS TEXTFILE;
--- src/main/webapp/app/components/form/pagination.constants.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    'use strict';
-
-    angular
-        .module('tfgJHipsterApp')
-        .constant('paginationConstants', {
-            'itemsPerPage': 20
-        });
-})();
