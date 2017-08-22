54d1ecd6b3b71ca73f525829bcc4d09b358ef771
Catalin <catalindumitrache76@yahoo.es>
2017-08-22 12:51:18 +0200

    Crawler working properly.

--- Crawlers_Scripts/crawler_productos_autorizados_españa.sh
+++ Crawlers_Scripts/crawler_productos_autorizados_españa.sh
@@ -1,34 +1,33 @@
 #!/usr/bin/env bash
-LOGFILE="/home/catalin/Escritorio/TFG/TFG/crawlerlog.log"
+LOGFILE="/home/catalin/Escritorio/TFG/TFG/Crawlers_Scripts/crawlerlog.log"
 FECHA="$(date +%Y_%m_%d-%H_%M)"
 FICHERO_DESCARGADO="$(echo "/home/catalin/Escritorio/TFG/Productos_fitosanitarios/España/Productos_autorizados/ListadoProductos_"${FECHA}".xls")"
-wget -O $FICHERO_DESCARGADO http://www.mapama.gob.es/es/agricultura/temas/sanidad-vegetal/productos-fitosanitarios/registro/productos/ListadoProductos.asp
+wget -O $FICHERO_DESCARGADO http://www.mapama.gob.es/es/agricultura/temas/sanidad-vegetal/productos-fitosanitarios/registro/productos/ListadoProductos.asp 2>> ${LOGFILE}
 if [ $? -eq 0 ]; then
-    FECHAEJECUCION="$(date +%Y_%m_%d-%H_%M)"
+    FECHAEJECUCION="$(date)"
     echo ${FECHAEJECUCION}": Se ha descargado el fichero con los productos autorizados" >> ${LOGFILE}
 else
     echo ${FECHAEJECUCION}": ERROR: No se ha descargado el fichero con los productos autorizados" >> ${LOGFILE}
 fi
-libreoffice --convert-to ods  --outdir /home/catalin/Escritorio/TFG/Productos_fitosanitarios/España/Productos_autorizados $FICHERO_DESCARGADO
+libreoffice --convert-to ods  --outdir /home/catalin/Escritorio/TFG/Productos_fitosanitarios/España/Productos_autorizados $FICHERO_DESCARGADO 2>> ${LOGFILE}
 if [ $? -eq 0 ]; then
-    FECHAEJECUCION="$(date +%Y_%m_%d-%H_%M)"
+    FECHAEJECUCION="$(date)"
     echo ${FECHAEJECUCION}": Se ha convertido el fichero viejo" >> ${LOGFILE}
 else
     echo ${FECHAEJECUCION}": ERROR: No se ha convertido el fichero viejo" >> ${LOGFILE}
 fi
 FICHERO_CONVERTIDO="$(echo "/home/catalin/Escritorio/TFG/Productos_fitosanitarios/España/Productos_autorizados/ListadoProductos_"${FECHA}".ods")"
-rm $FICHERO_DESCARGADO
+rm $FICHERO_DESCARGADO 2>> ${LOGFILE}
 if [ $? -eq 0 ]; then
-    FECHAEJECUCION="$(date +%Y_%m_%d-%H_%M)"
+    FECHAEJECUCION="$(date)"
     echo ${FECHAEJECUCION}": Se ha borrado el fichero viejo" >> ${LOGFILE}
 else
     echo ${FECHAEJECUCION}": ERROR: No se ha borrado el fichero viejo" >> ${LOGFILE}
 fi
-/home/catalin/Escritorio/TFG/HADOOP/hadoop-2.8.0/bin/hadoop fs -put $FICHERO_CONVERTIDO /user/TFG/Productos_fitosanitarios/España/Productos_autorizados
+/home/catalin/Escritorio/TFG/HADOOP/hadoop-2.8.0/bin/hadoop fs -put $FICHERO_CONVERTIDO /user/TFG/Datos_en_crudo/España/Productos_autorizados 2>> ${LOGFILE}
 if [ $? -eq 0 ]; then
-    EJECUCION=$?
-    FECHAEJECUCION="$(date +%Y_%m_%d-%H_%M)"
+    FECHAEJECUCION="$(date)"
     echo ${FECHAEJECUCION}": Se ha copiado el fichero en Hadoop. " >> ${LOGFILE}
 else
-    echo ${FECHAEJECUCION}": ERROR: No se ha copiado el fichero en Hadoop. Código: "${EJECUCION} >> ${LOGFILE}
+    echo ${FECHAEJECUCION}": ERROR: No se ha copiado el fichero en Hadoop. Código: "$? >> ${LOGFILE}
 fi
