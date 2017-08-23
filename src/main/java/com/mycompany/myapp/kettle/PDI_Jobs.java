b19e48b79d72458b0da7dc06c8dd4cdabca4c38a
Catalin <catalindumitrache76@yahoo.es>
2017-08-23 14:11:19 +0200

    KTR simple job functional (outputs to a text file); Hadoop job still not working;

--- src/main/java/com/mycompany/myapp/kettle/PDI_Jobs.java
+++ src/main/java/com/mycompany/myapp/kettle/PDI_Jobs.java
@@ -5,6 +5,8 @@ import org.pentaho.di.core.plugins.PluginFolder;
 import org.pentaho.di.core.plugins.StepPluginType;
 import org.pentaho.di.trans.TransMeta;
 import org.pentaho.di.trans.Trans;
+
+
 /**
  * Created by catalin on 17/08/17.
  */
@@ -12,12 +14,25 @@ import org.pentaho.di.trans.Trans;
 public class PDI_Jobs {
 
     public static void main (String[] args) {
+        // funciona = true -> Se ejecuta el job que realiza output a un fichero de texto
+        // funciona = false -> Se ejecuta el job que realiza output a Hadoop (no funciona)
+        boolean funciona = true;
         try {
             StepPluginType.getInstance().getPluginFolders().add(new PluginFolder("/home/catalin/Escritorio/TFG/Kettle/plugins", false, true));
+            StepPluginType.getInstance().getPluginFolders().add(new PluginFolder("/home/catalin/Escritorio/TFG/Plugins_Kettle", false, true));
 
             KettleEnvironment.init();
 
-            TransMeta transMeta = new TransMeta("Kettle_Scripts/T1.ktr");
+            TransMeta transMeta;
+
+            if (funciona) {
+                // Este funciona a pesar de mostrar muchos mensajes de error en la consola
+                transMeta = new TransMeta("Kettle_Scripts/A_fichero_externo.ktr");
+            }else {
+                //Este no funciona porque le falta un plugin para hadoop ... creo que el de pentaho-big-data-plugin
+                transMeta = new TransMeta("Kettle_Scripts/A_Hadoop.ktr");
+            }
+
             Trans trans = new Trans(transMeta);
 
             trans.execute(null); // You can pass arguments instead of null.
