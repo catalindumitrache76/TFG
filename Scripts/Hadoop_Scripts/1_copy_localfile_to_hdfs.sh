76e0fa6a972be8b7384e94caebe760c2feab2d5e
Catalin <catalindumitrache76@yahoo.es>
2017-11-03 15:32:06 +0100

    updated scripts for genericity purpose

--- Scripts/Hadoop_Scripts/1_copy_localfile_to_hdfs.sh
+++ Scripts/Hadoop_Scripts/1_copy_localfile_to_hdfs.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-hadoop fs -put $(../getProperty.sh dir_proyecto_1) + "Prueba_concepto/Listado_Productos/ListadoProductos_2017_02_13-15_59_con_id.csv" /user/data/prueba_concepto
+hadoop fs -put $(echo $(./../getProperty.sh dir_proyecto_1)"Prueba_concepto/Listado_Productos/ListadoProductos_2017_02_13-15_59_con_id.csv") /user/data/prueba_concepto
